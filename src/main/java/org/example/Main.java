974b3c48ee71b9eddad2ec2cf386889ee5c2f2b8
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-07-15 13:53:13 +0200

    add simple crud in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,59 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.example.dao.RegionDao;
+import org.example.enums.Climate;
+import org.example.entity.RegionEntity;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("regionPU");
+        EntityManager em = emf.createEntityManager();
+        RegionDao regionDao = new RegionDao(em);
+
+        try {
+            RegionEntity region = createRegion(regionDao);
+            printAllRegions(regionDao);
+            updateRegion(regionDao, region);
+            deleteRegion(regionDao, region.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
         }
+    }
+
+    private static RegionEntity createRegion(RegionDao dao) {
+        RegionEntity region = new RegionEntity();
+        region.setName("Sahara");
+        region.setSurface(9100000);
+        region.setClimate(Climate.ARID);
+        dao.create(region);
+        System.out.println("Created region: " + region);
+        return region;
+    }
+
+    private static void printAllRegions(RegionDao dao) {
+        List<RegionEntity> regions = dao.findAll();
+        System.out.println("All regions:");
+        regions.forEach(System.out::println);
+    }
+
+    private static void updateRegion(RegionDao dao, RegionEntity region) {
+        region.setName("Great Sahara");
+        dao.update(region);
+        RegionEntity updated = dao.findById(region.getId());
+        System.out.println("Updated region: " + updated);
+    }
+
+    private static void deleteRegion(RegionDao dao, Long id) {
+        dao.delete(id);
+        System.out.println("Deleted region with ID: " + id);
     }
 }
