3c5c6160e037e8426abdd974db00feed43637d6b
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-07-15 13:41:07 +0200

    add dao

--- src/main/java/org/example/dao/RegionDao.java
+++ src/main/java/org/example/dao/RegionDao.java
@@ -1,4 +1,44 @@
 package org.example.dao;
 
+import org.example.entity.RegionEntity;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class RegionDao {
+
+    private final EntityManager em;
+
+    public RegionDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void create(RegionEntity region) {
+        em.getTransaction().begin();
+        em.persist(region);
+        em.getTransaction().commit();
+    }
+
+    public RegionEntity findById(Long id) {
+        return em.find(RegionEntity.class, id);
+    }
+
+    public List<RegionEntity> findAll() {
+        return em.createQuery("FROM Region", RegionEntity.class).getResultList();
+    }
+
+    public void update(RegionEntity region) {
+        em.getTransaction().begin();
+        em.merge(region);
+        em.getTransaction().commit();
+    }
+
+    public void delete(Long id) {
+        RegionEntity region = em.find(RegionEntity.class, id);
+        if (region != null) {
+            em.getTransaction().begin();
+            em.remove(region);
+            em.getTransaction().commit();
+        }
+    }
 }
